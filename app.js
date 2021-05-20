4a8c9eecd04acfb87f3de0af9cb6e5dc5ed1954f
oskytar <oscar.miras@gmail.com>
2021-05-20 19:04:36 +0200

    fix solution

--- app.js
+++ app.js
@@ -3,32 +3,33 @@ const http = require('http')
 const url = require('url');
 
 const app = http.createServer((req, res) => {
-    
+
     // Sin esta configuración, no podríamos acceptar peticiones desde otro servidor y/o puerto. Esta configuración dice: "Acepta peticiones de otro script; venga de donde venga"
 
     res.setHeader('Access-Control-Allow-Origin', '*');
-    
+
     if (req.url.includes('/color')) {
+      
         // hay parámetros de QueryString
         const adr = req.host + req.url;
         const q = url.parse(adr, true)
 
-        let resultColor = colors[0];
+        let resultColor;
 
-        if (q.query.variant) {
-            res.writeHead(200, { 'Content-Type': 'text/plain' })
-            switch (q.query.variant) {
-                case 'red':
-                    resultColor = colors[0]
-                    break;
-                case 'green':
-                    resultColor = colors[1];
-                    break;
-                case 'blue':
-                    resultColor = colors[2];
-                    break;
-            }
-
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+      
+        switch (q.query.variant) {
+            case 'red':
+                resultColor = colors[0]
+                break;
+            case 'green':
+                resultColor = colors[1];
+                break;
+            case 'blue':
+                resultColor = colors[2];
+                break;
+            default:
+                resultColor = colors[Math.floor(Math.random()*colors.length)];
         }
 
         res.write(resultColor)
@@ -37,7 +38,7 @@ const app = http.createServer((req, res) => {
     }
 
     else {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
         res.write(`
             <h1>Bienvenidos a la base de datos de colores de NetMind!</h1>
             <p>Para obtener un color aleatório; tan solo debes hacer una petición GET al endpoint <strong>/color'</strong> </p>
